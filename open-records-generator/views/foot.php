24a84bc9ff708ee4a2e347e577433ec217dbec83
lily healey <lily@lilyhealey.co.uk>
2016-01-19 10:59:24 -0500

    add open-records-generator submodule

--- open-records-generator/views/foot.php
+++
@@ -1,9 +0,0 @@
-			<div id="footer-container" class="flex-min">
-				<footer class="centre">
-						<a class="button" href="<? echo $admin_path; ?>info">INFO</a>
-						<a class="button" href="<? echo $host; ?>" target="_blank">GENERATE</a>
-				</footer>
-			</div>
-		</div>
-	</body>
-</html>
