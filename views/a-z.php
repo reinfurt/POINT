2070da9b6412e5daab3ae0ec828b1d61ae1ae355
lily healey <lily@lilyhealey.co.uk>
2016-04-20 10:27:05 -0400

    alphabetise

--- views/a-z.php
+++ views/a-z.php
@@ -1,27 +1,55 @@
 <div class="now">
-	<div id="a-z-select">A–Z</div>
+    <div id="a-z-select">A–Z</div>
 </div>
-	
+    
 <div id="a-z" class="now"><?
-	$report_id = 6;
-	$u = $oo->get($report_id)['url'];
-	$reports = $oo->children($report_id);
-	foreach($reports as $r)
-	{
-		$url = $host.$u."/".$r['url'];
-		?><div><a href="<? echo $url; ?>"><? echo $r['name1']; ?></a></div><?
-	}
-	?></div>
-	<script>
-	 	a_z_select = document.getElementById("a-z-select");
-		a_z_select.onclick = 
-		function()
-		{
-			a_z = document.getElementById("a-z");
-			if(a_z.style.display == "block")
-				a_z.style.display = "none";		
-			else
-				a_z.style.display = "block";
-			};
-	</script>
+    $report_ids = array(3, 4, 5, 6);
+    // make (wires.fromid = 3 OR wires.fromid = 4 . . . )
+    $rsql = "(wires.fromid = ".$report_ids[0];
+    for($i = 1; $i < count($report_ids); $i++)
+    {
+        $rid = $report_ids[$i];
+        $rsql.= " OR wires.fromid = $rid";
+    }
+    $rsql.= ")";
+    $sql = "SELECT 
+                objects.*,
+                wires.fromid
+            FROM
+                objects, wires
+            WHERE
+                objects.active = 1
+                AND wires.active = 1
+                AND wires.toid = objects.id
+                AND $rsql
+            ORDER BY
+                objects.name1";
+    $res = $db->query($sql);
+    $reports = array();
+    while($obj = $res->fetch_assoc())
+        $reports[] = $obj;
+    $res->close(); 
+    $report_id = 6;
+    // $u = "annual-reports/".$oo->get($report_id)['url'];
+    // $reports = $oo->children($report_id);
+    foreach($reports as $r)
+    {
+        // this is extremely inefficient -- how to make it better?
+        $u = "annual-reports/".$oo->get($r['fromid'])['url'];
+        $url = $host.$u."/".$r['url'];
+        ?><div><a href="<? echo $url; ?>"><? echo $r['name1']; ?></a></div><?
+    }
+    ?></div>
+    <script>
+        a_z_select = document.getElementById("a-z-select");
+        a_z_select.onclick = 
+        function()
+        {
+            a_z = document.getElementById("a-z");
+            if(a_z.style.display == "block")
+                a_z.style.display = "none";     
+            else
+                a_z.style.display = "block";
+            };
+    </script>
 </div>
--- views/event.php
+++ views/event.php
@@ -2,57 +2,70 @@
 if($uu->id)
 {
 ?><div id="close">
-	<a href="<? echo $js_back; ?>"><img src="<? echo $media_path; ?>svg/ex.svg"></a>
+    <a href="<? echo $js_back; ?>"><img src="<? echo $media_path; ?>svg/ex.svg"></a>
 </div><?
-}	
+}   
 ?><script type="text/javascript" src="<? echo $host; ?>static/js/animate-message.js"></script>
-<div id="page" class="column"><?
-	if($uu->id)
-	{
-	?><div id="event-wrapper">
-		<div id="event-display"><?
-		$m_arr = $oo->media($uu->id);
-		$b_arr = process_event($item['name1']." / ".$item['body']);
-		for($i = 0; $i < count($b_arr); $i++)
-		{
-			echo $b_arr[$i];
-			if($m_arr[$i])
-			{
-			?><div class="img-container"><? 
-				$ii = $i;
-				while ($m_arr[$ii] != null) { ?>
-					<img src="<? echo m_url($m_arr[$ii]); ?>"><?
-					$ii++;
-				}
-			?></div><?
-			}
-		}
-		$report_url = $host.implode("/", array_slice($uu->urls, 0, count($uu->urls)-1));
-		?><div class="now"><a href="<? echo $report_url; ?>">Go back to <? echo $year; ?>. . . </a></div>
-		</div>
-	</div><?
-	}
-	else
-	{
-	?><div class="now">
-		<p>Meanwhile, here is what’s happening now:</p>
-		<div id="ticker-wrapper">
-			<div id="ticker-display"></div>
-			<div id="ticker-source" class="hidden"><?					
-			foreach($now_children as $c)
-			{
-				echo process_md($c['name1']." / ".$c['deck']." / ".$c['body']);
-			}
-			?></div>
-		</div>
-	</div>
-	<script type="text/javascript">
-		//var animate = !(checkCookie("animateCookie"));
-		//setCookie("animateCookie");
-		animate = true;
-		tickerDelay = 40;
-		document.onload = initMessage("ticker-source","ticker-display",animate,tickerDelay);
-	</script><?
-	}
-	?>
+<?
+if ($uu->id)
+{
+    $m_arr = $oo->media($uu->id);
+    if ($m_arr[0])
+    {
+    ?><div id="img-display">
+        <img src="<? echo m_url($m_arr[0]); ?>">
+    </div><?
+    }
+}
+?><div id="page" class="column"><?
+    if($uu->id)
+    {
+    ?><div id="event-wrapper">
+        <div id="event-display"><?
+       
+        $b_arr = process_event($item['name1']." / ".$item['body']);
+        for($i = 0; $i < count($b_arr); $i++)
+        {
+            echo $b_arr[$i];
+            if($m_arr[$i])
+            {
+            ?><div class="img-container"><? 
+                $ii = $i;
+                while ($m_arr[$ii] != null) { ?>
+                    <img src="<? echo m_url($m_arr[$ii]); ?>"><?
+                    $ii++;
+                }
+            ?></div><?
+            }
+        }
+        $report_url = $host.implode("/", array_slice($uu->urls, 0, count($uu->urls)-1));
+            ?><div class="now">
+                <a href="<? echo $report_url; ?>">Go back to <? echo $year; ?>. . . </a>
+            </div>
+        </div>
+    </div><?
+    }
+    else
+    {
+    ?><div class="now">
+        <p>Meanwhile, here is what’s happening now:</p>
+        <div id="ticker-wrapper">
+            <div id="ticker-display"></div>
+            <div id="ticker-source" class="hidden"><?                   
+            foreach($now_children as $c)
+            {
+                echo process_md($c['name1']." / ".$c['deck']." / ".$c['body']);
+            }
+            ?></div>
+        </div>
+    </div>
+    <script type="text/javascript">
+        //var animate = !(checkCookie("animateCookie"));
+        //setCookie("animateCookie");
+        animate = true;
+        tickerDelay = 40;
+        document.onload = initMessage("ticker-source","ticker-display",animate,tickerDelay);
+    </script><?
+    }
+    ?>
 </div>
