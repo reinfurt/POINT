4676ac531ac873cc55ccbc3878c8af60202629dc
david reinfurt <reinfurt@Adriano-Olivetti.local>
2017-05-29 15:20:28 +0200

    year-menu with rollover added and working

--- views/year-menu.php
+++ views/year-menu.php
@@ -1,48 +1,49 @@
-<?
+<? 
+// year menu
+// show all years on index, only selected on event
+// order years, no repeats
 
-// parse url, get $thisyear
 $thisyear = $uu->urls[1];
-if(!intval($thisyear))
+if(intval($thisyear))
+    $thisyearobject = $oo->get($uu->id);
+else
     $thisyear = null;
 
-// build #year-menu
-// find "annual reports" object
+// find "annual reports" & harvest years
 $fields = array("id, name1");
 $tables = array("objects");
 $where = array("name1 LIKE 'Annual Reports'");
-$order = array();
+$order = array();   // order by rank?
 $limit = "1";
 $descending = TRUE;
 $distinct = TRUE;
 $annualreports = $oo->get_all($fields, $tables, $where, $order, $limit, $descending, $distinct);
 $annualreportsid = $annualreports[0]['id'];
 $years = $oo->nav(["$annualreportsid"], $annualreportsid);    // nav needs ids array, rootid int
+$yearsinorder = array();    
+if ($thisyear) 
+    $yearsinorder[0]['o'] = $thisyearobject;
+foreach($years as $year)
+    if ($year['o']['name1'] != $thisyear)
+        array_push($yearsinorder, $year);
 
-print_r($years[0]['o']['name1']);
-                                                    // not $uu page-specific
-// build & output html
-?><div id="year-menu" class="h-centre"><?
-foreach($years as $year) {                      
-	$y_arr = str_split($year['o']['name1']);
+// output html
+?><div id="year-menu" class="h-centre <? echo ($thisyear) ? 'hide-overflow' : 'show-overflow'; ?>"><?
+foreach($yearsinorder as $year) {
+	$year_digits = str_split($year['o']['name1']);
     $urls = array();
-	foreach($y_arr as $d)
+	foreach($year_digits as $d)
 		$urls[] = $media_path."png/$d.png";
     $urlpoint = $media_path."png/point.png";
-    
     ?><a href="<? echo $host . "annual-reports/" . $year['o']['name1']; ?>"><?
-    ?><div class="year-container blink"><?
-    foreach($urls as $u) {
-        ?><div class="digit-container">
-            <img src="<? echo $u; ?>">
-        </div><?
-    }
-    ?><span id="dot" class="blink"><img src="<? echo $urlpoint; ?>"></span><?
-    ?></div><?
+        ?><div class="year-container blink"><?
+        foreach($urls as $u) {
+            ?><div class="digit-container">
+                <img src="<? echo $u; ?>">
+            </div><?
+        }
+        ?><span id="dot" class="blink"><img src="<? echo $urlpoint; ?>"></span><?
+        ?></div><?
     ?></a><?
-
 }
 ?></div>
-
-<!--
-// turn overflow on or off depending if in a year or on home
--->
